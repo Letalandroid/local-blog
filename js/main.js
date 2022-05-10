92755eda32cea061a8eb8b156db7e000f98edce5
Letalandroid <carlossoncra@gmail.com>
2022-05-10 11:05:27 -0500

    v1.0.0

--- js/main.js
+++ js/main.js
@@ -4,19 +4,53 @@ document.querySelector(".add").addEventListener ("click", () => {
 
 });
 
-for (let i = 0; i <= localStorage.getItem("lastedBlog") - 0; i++) {
+
+for (let i = 0; i <= JSON.parse(localStorage.getItem("titles")).length - 1; i++) {
 
     const blog = document.createElement("section");
 
     blog.innerHTML = `
 
-        <section>
-            <h2>${localStorage.getItem(`blogTitle${i}`)}</h2>
-            <p>${localStorage.getItem(`blogDesc${i}`)}</p>
-        </section>
+        <h2>${JSON.parse(localStorage.getItem("titles"))[i]}</h2>
+        <hr>
+        <p>${JSON.parse(localStorage.getItem("descs"))[i]}</p>
 
     `;
 
     document.querySelector("main").appendChild(blog);
 
 }
+
+document.getElementById("dark-mode").addEventListener("change", () => {
+
+    if (!document.getElementById("dark-mode").checked) {
+
+        document.body.style.backgroundColor = "#fff";
+
+        for (let i = 0; i <= JSON.parse(localStorage.getItem("titles")).length - 1; i++) {
+
+            document.querySelectorAll("section")[i].style.backgroundColor = "#fff";
+            document.querySelectorAll("section")[i].style.boxShadow = "3px 3px 5px 0px #000a";
+            document.querySelectorAll("h2")[i].style.color = "#000";
+            document.querySelectorAll("p")[i].style.color = "#000";
+            document.querySelectorAll("hr")[i].style.border = "1px solid #444";
+
+        }
+
+    } else {
+
+        document.body.style.backgroundColor = "#141414";
+
+        for (let i = 0; i <= JSON.parse(localStorage.getItem("titles")).length - 1; i++) {
+
+            document.querySelectorAll("section")[i].style.backgroundColor = "#282828";
+            document.querySelectorAll("section")[i].style.boxShadow = "3px 3px 5px 0px #fffa";
+            document.querySelectorAll("h2")[i].style.color = "#fff";
+            document.querySelectorAll("p")[i].style.color = "#fff";
+            document.querySelectorAll("hr")[i].style.border = "1px solid #fff";
+
+        }
+
+    }
+
+});
--- new-post/js/main.js
+++ new-post/js/main.js
@@ -1,19 +1,83 @@
-localStorage.setItem("lastedBlog", 0);
+let send = document.getElementById("send");
+let title = document.getElementById("title");
+let desc = document.getElementById("desc");
 
-document.querySelector("#send").addEventListener("click", () => {
+if (localStorage.length == 0) {
 
-    localStorage.setItem(`blogTitle${localStorage.getItem("lastedBlog")}`, document.getElementById("title").value);
-    localStorage.setItem(`blogDesc${localStorage.getItem("lastedBlog")}`, document.getElementById("desc").value);
+    let titles = [];
+    let descs = [];
 
-    if (localStorage.getItem("lastedBlog") > 0) {
+    send.addEventListener("click", () => {
 
-        localStorage.setItem("lastedBlog", 1);
+        titles.push(title.value);
+        descs.push(desc.value);
+
+        localStorage.setItem("titles", JSON.stringify(titles));
+        localStorage.setItem("descs", JSON.stringify(descs));
+
+        window.location.href = "../"
+
+    });
+
+} else {
+
+    const object1 = localStorage.getItem("titles");
+    const object2 = localStorage.getItem("descs");
+    let titles = JSON.parse(object1);
+    let descs = JSON.parse(object2);
+
+    send.addEventListener("click", () => {
+
+        titles.push(title.value);
+        descs.push(desc.value);
+
+        localStorage.setItem("titles", JSON.stringify(titles));
+        localStorage.setItem("descs", JSON.stringify(descs));
+
+        window.location.href = "../"
+
+    });
+
+}
+
+document.getElementById("dark-mode").addEventListener("change", () => {
+
+    if (!document.getElementById("dark-mode").checked) {
+
+        document.body.style.backgroundColor = "#fff";
+
+        for (let i = 0; i <= 1; i++) {
+
+            document.querySelectorAll("label")[i].style.color = "#000";
+            document.getElementById("title").style.color = "#000"
+            document.getElementById("title").style.background = "#fff"
+            document.getElementById("title").style.outline = "2.5px solid var(--color-primary)"
+            document.getElementById("desc").style.color = "#000"
+            document.getElementById("desc").style.background = "#fff"
+            document.getElementById("desc").style.outline = "2.5px solid var(--color-primary)"
+            document.getElementById("send").style.color = "#000"
+            document.getElementById("send").style.background = "var(--color-secundary)"
+
+        }
 
     } else {
-        
-        localStorage.setItem("lastedBlog", localStorage.getItem("lastedBlog") + 1);
-    }
 
-    window.location.href = "../";
+        document.body.style.backgroundColor = "#141414";
+
+        for (let i = 0; i <= 1; i++) {
+
+            document.querySelectorAll("label")[i].style.color = "#fff";
+            document.getElementById("title").style.color = "#fff"
+            document.getElementById("title").style.background = "#282828"
+            document.getElementById("title").style.outline = "2.5px solid var(--color-secundary)"
+            document.getElementById("desc").style.color = "#fff"
+            document.getElementById("desc").style.background = "#282828"
+            document.getElementById("desc").style.outline = "2.5px solid var(--color-secundary)"
+            document.getElementById("send").style.color = "#fff"
+            document.getElementById("send").style.background = "var(--color-primary)"
+
+        }
+
+    }
 
 });
